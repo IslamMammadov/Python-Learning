b60aa7f3bbff74d72eef116ea1349dcf7d03e100
Islam <60185596+IslamMammadov@users.noreply.github.com>
2023-11-28 19:28:14 +0400

    Update Collatz hypothesi.py

--- Collatz hypothesi.py
+++ Collatz hypothesi.py
@@ -1,3 +1,11 @@
+"""
+1) take any non-negative and non-zero integer number and name it c0;
+2) if it's even, evaluate a new c0 as c0 ÷ 2;
+3) otherwise, if it's odd, evaluate a new c0 as 3 × c0 + 1;
+4) if c0 ≠ 1, skip to point 2.
+"
+
+
 c0=int(input("Enter number: "))
 steps=0
 while True:
